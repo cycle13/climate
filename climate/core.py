1359cfb660987f6c6463b9e7557d7108eb6abbce
tg359 <tristan.gerrish@burohappold.com>
2019-12-02 15:16:24 +0000

    Added localisation to datetimeindex

--- climate/core.py
+++ climate/core.py
@@ -1,12 +1,48 @@
 import os
-from io import StringIO
 import subprocess
-
+from io import StringIO
 import pandas as pd
 import numpy as np
 
 from .helpers import *
 
+
+class BuroHappold(object):
+
+    def __init__(self):
+
+        self.colors = {
+            "official": {
+                "BrightGreen": [108, 194, 78],
+                "BrightPurple": [112, 47, 138],
+                "BrightRed": [213, 0, 50],
+                "DarkBlue": [0, 97, 127],
+                "DarkRed": [126, 45, 64],
+                "Green": [34, 136, 72],
+                "Grey": [150, 140, 131],
+                "LightBlue": [141, 185, 202],
+                "LightRed": [188, 32, 75],
+                "Orange": [255, 143, 28],
+                "Pink": [231, 130, 169],
+                "Purple": [36, 19, 95],
+                "BrandGreen": [196, 214, 0],
+                "Teal": [0, 164, 153],
+            },
+            "ppt": {
+                "Purple": [143, 114, 176],
+                "BrandGreen": [196, 214, 0],
+                "Pink": [230, 49, 135],
+                "LightBlue": [0, 176, 240],
+                "DarkBlue": [28, 54, 96],
+                "Orange": [235, 103, 28],
+                "DarkPurple": [85, 62, 112],
+                "DarkGreen": [118, 128, 0],
+                "DarkPink": [149, 18, 80],
+                "Blue": [0, 106, 144],
+                "ReallyDarkBlue": [17, 32, 58],
+                "Brown": [145, 61, 13],
+            },
+        }
 
 class EPW(object):
 
@@ -15,60 +51,67 @@ class EPW(object):
         # Metadata
         self.filepath = os.path.abspath(filepath) if filepath is not None else None
 
-        # Header information
-        self.city = ''
-        self.region = ''
-        self.country = ''
-        self.dataset_type = ''
-        self.station_id = ''
-        self.latitude = []
-        self.longitude = []
-        self.elevation = 0
-        self.time_zone = 0
-        self.design_conditions = ''
-        self.typical_extreme_periods = ''
-        self.ground_temperatures = ''
-        self.holidays_daylight_savings = ''
-        self.comments_1 = ''
-        self.comments_2 = ''
+        # Location variables
+        self.city = None
+        self.region = None
+        self.country = None
+        self.dataset_type = None
+        self.station_id = None
+        self.latitude = None
+        self.longitude = None
+        self.elevation = None
+        self.time_zone = None
+        self.design_conditions = None
+        self.typical_extreme_periods = None
+        self.ground_temperatures = None
+        self.holidays_daylight_savings = None
+        self.comments_1 = None
+        self.comments_2 = None
 
-        # Variables information
-        self.year = []
-        self.month = []
-        self.day = []
-        self.hour = []
-        self.minute = []
-        self.data = None
-        self.data_source_and_uncertainty_flags = []
-        self.dry_bulb_temperature = []
-        self.dew_point_temperature = []
-        self.relative_humidity = []
-        self.atmospheric_station_pressure = []
-        self.extraterrestrial_horizontal_radiation = []
-        self.extraterrestrial_direct_normal_radiation = []
-        self.horizontal_infrared_radiation_intensity = []
-        self.global_horizontal_radiation = []
-        self.direct_normal_radiation = []
-        self.diffuse_horizontal_radiation = []
-        self.global_horizontal_illuminance = []
-        self.direct_normal_illuminance = []
-        self.diffuse_horizontal_illuminance = []
-        self.zenith_luminance = []
-        self.wind_direction = []
-        self.wind_speed = []
-        self.total_sky_cover = []
-        self.opaque_sky_cover = []
-        self.visibility = []
-        self.ceiling_height = []
-        self.present_weather_observation = []
-        self.present_weather_codes = []
-        self.precipitable_water = []
-        self.aerosol_optical_depth = []
-        self.snow_depth = []
-        self.days_since_last_snowfall = []
-        self.albedo = []
-        self.liquid_precipitation_depth = []
-        self.liquid_precipitation_quantity = []
+        # Series variables
+        self.df = None
+        self.data_source_and_uncertainty_flags = None
+        self.dry_bulb_temperature = None
+        self.dew_point_temperature = None
+        self.relative_humidity = None
+        self.atmospheric_station_pressure = None
+        self.extraterrestrial_horizontal_radiation = None
+        self.extraterrestrial_direct_normal_radiation = None
+        self.horizontal_infrared_radiation_intensity = None
+        self.global_horizontal_radiation = None
+        self.direct_normal_radiation = None
+        self.diffuse_horizontal_radiation = None
+        self.global_horizontal_illuminance = None
+        self.direct_normal_illuminance = None
+        self.diffuse_horizontal_illuminance = None
+        self.zenith_luminance = None
+        self.wind_direction = None
+        self.wind_speed = None
+        self.total_sky_cover = None
+        self.opaque_sky_cover = None
+        self.visibility = None
+        self.ceiling_height = None
+        self.present_weather_observation = None
+        self.present_weather_codes = None
+        self.precipitable_water = None
+        self.aerosol_optical_depth = None
+        self.snow_depth = None
+        self.days_since_last_snowfall = None
+        self.albedo = None
+        self.liquid_precipitation_depth = None
+        self.liquid_precipitation_quantity = None
+
+        # Derived variables
+        self.solar_altitude = None
+        self.solar_azimuth = None
+        self.solar_wet_bulb_temperature = None
+        self.solar_enthalpy = None
+        self.solar_humidity_ratio = None
+        self.universal_thermal_climate_index = None
+        self.standard_effective_temperature = None
+        self.solar_adjusted_mean_radiant_temperature = None
+        self.mean_radiant_temperature = None
+
 
     def read(self):
         with open(self.filepath, "r") as f:
@@ -80,21 +123,28 @@ class EPW(object):
             self.longitude = float(self.longitude)
             self.time_zone = float(self.time_zone)
             self.elevation = float(self.elevation)
+
             # Read design conditions data
             self.design_conditions = ",".join(dat[1].strip().split(",")[1:])
+
             # Read typical extreme periods data
             self.typical_extreme_periods = ",".join(dat[2].strip().split(",")[1:])
+
             # Read ground temperatures data
             self.ground_temperatures = ",".join(dat[3].strip().split(",")[1:])
+
             # Read holidays/daylight savings data
             self.holidays_daylight_savings = ",".join(dat[4].strip().split(",")[1:])
+
             # Read comments 1 data
             self.comments_1 = ",".join(dat[5].strip().split(",")[1:])
+
             # Read comments 2 data
             self.comments_2 = ",".join(dat[6].strip().split(",")[1:])
 
             # Read the data table
             df = pd.read_csv(StringIO("\n".join(dat[8:])), header=None)
+
             # Rename columns
             df.columns = [
                 'year',
@@ -116,7 +166,8 @@ class EPW(object):
                 'global_horizontal_illuminance',
                 'direct_normal_illuminance',
                 'diffuse_horizontal_illuminance',
-                'zenith_luminance', 'wind_direction',
+                'zenith_luminance',
+                'wind_direction',
                 'wind_speed',
                 'total_sky_cover',
                 'opaque_sky_cover',
@@ -132,12 +183,18 @@ class EPW(object):
                 'liquid_precipitation_depth',
                 'liquid_precipitation_quantity',
             ]
+
             # Create datetime index - using 2018 as base year (a Monday starting year without leap-day)
-            df.index = pd.date_range(start="2018-01-01 00:00:00", end="2019-01-01 00:00:00", freq="60T", closed="left")#.tz_localize(tz=self.time_zone)
-            self.dt = df.index
+            df.index = pd.date_range(
+                start="2018-01-01 00:00:00",
+                end="2019-01-01 00:00:00",
+                freq="60T",
+                closed="left").tz_localize("UTC").tz_convert(int(self.time_zone * 60 * 60))
+
+            # Drop date/time columns
             df.drop(columns=["year", "month", "day", "hour", "minute"], inplace=True)
 
-            # Make loaded data accesible
+            # Make loaded data accessible
             self.df = df
             self.data_source_and_uncertainty_flags = df.data_source_and_uncertainty_flags
             self.dry_bulb_temperature = df.dry_bulb_temperature
@@ -170,3 +227,7 @@ class EPW(object):
             self.liquid_precipitation_depth = df.liquid_precipitation_depth
             self.liquid_precipitation_quantity = df.liquid_precipitation_quantity
 
+
+    def to_csv(self, filepath):
+        self.df.to_csv(filepath)
+        return filepath
--- climate/helpers.py
+++ climate/helpers.py
@@ -1,9 +1,50 @@
-import ast
+import os
+
+##########################
+# Generic helper methods #
+##########################
 
 
-def evaluate(val):
+def interpret(val):
     try:
-        val = ast.literal_eval(val)
+        return int(val)
     except ValueError:
-        pass
-    return val
+        try:
+            return float(val)
+        except ValueError:
+            return val
+
+
+def chunk(enumerable, n_chunks):
+    for i in range(0, len(enumerable), n_chunks):
+        yield enumerable[i:i + n_chunks]
+
+
+def generate_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
+
+def hex_to_rgb(hex_string):
+    hex_string = hex_string.lstrip('#')
+    lv = len(hex_string)
+    return tuple(int(hex_string[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def rgb_to_hex(rgbList):
+    if (rgbList[0] >= 1) | (rgbList[1] >= 1) | (rgbList[2] >= 1):
+        return '#%02x%02x%02x' % (int(rgbList[0]), int(rgbList[1]), int(rgbList[2]))
+    else:
+        return '#%02x%02x%02x' % (int(rgbList[0] * 255), int(rgbList[1] * 255), int(rgbList[2] * 255))
+
+
+def celsius_to_kelvin(tc):
+    return tc + 273.15
+
+
+#########################
+# Psychrometric methods #
+#########################
+
+
